038d97f13726dcdcd2b55b9215e74dc37ded3da7
robertjosephwayne <robertjwayne@gmail.com>
2017-04-20 17:50:17 -0400

    Create query to select author names and their series

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,9 +23,16 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
+  "SELECT authors.name, subgenres.name
+  FROM series
+  INNER JOIN authors ON series.author_id = authors.id
+  INNER JOIN subgenres ON series.subgenre_id = subgenres.id;"
+  selects the authors names and their series' subgenres
+
 end
 
 def select_series_title_with_most_human_characters
+
 end
 
 def select_character_names_and_number_of_books_they_are_in
